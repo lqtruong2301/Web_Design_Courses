229f34a2cfbe965e7b2200e47c81a6fa2e00e7f0
Minh <minh09az80@gmail.com>
2024-06-12 00:21:00 +0700

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import HelloWorld from "./pages/HelloWorld/HelloWorld";
-
+import Test from "./pages/Test/Test";
 function App() {
     const [count, setCount] = useState(0);
 
@@ -10,6 +10,7 @@ function App() {
         <>
             <Routes>
                 <Route path="/hello" element={<HelloWorld />} />
+                <Route path="/" element={<Test />} />
             </Routes>
         </>
     );
